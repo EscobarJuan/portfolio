9bafc87eeb35d792c33a78bc82bde8625492ba58
Juan <escobar130485@gmail.com>
2022-05-09 11:37:02 -0300

    MedTemp v9

--- src/Components/Proyectos.js
+++ src/Components/Proyectos.js
@@ -12,18 +12,20 @@ function Proyectos(props){
     console.log(props);
     return(
         <div>
-            <Card sx={{ bgcolor:"#D2D2E3", height:'50vh'}}>
-                
-                    {props.src == "http://www.escuelasuperiordeguerra.iese.edu.ar/" ?  
-                           <ResponsiveEmbed src={props.src}  allowFullScreen/> 
-                        : 
-                        <Card sx={{ bgcolor:"#D2D2E3", border:2, borderColor:'darkblue'}}>
-                            <CardMedia sx={{maxWidth:800, m:0, ml:'auto', mr:'auto'}}>
-                                <ResponsiveEmbed src={props.src}  ratio = ' 3:1.5 '/>  
-                            </CardMedia>
-                        </Card>
-                    }
-               
+            <Card sx={{ bgcolor:"#D2D2E3", height:'100vh'}}>
+                {props.src == "http://www.escuelasuperiordeguerra.iese.edu.ar/" ?  
+                    <Card sx={{ bgcolor:"#D2D2E3", border:2, borderColor:'darkblue'}}>
+                        <CardMedia sx={{maxWidth:800, m:0, ml:'auto', mr:'auto'}}>
+                            <ResponsiveEmbed src={props.src}  ratio = ' 3:1 '/>  
+                        </CardMedia>
+                    </Card>
+                    : 
+                    <Card sx={{ bgcolor:"#D2D2E3", border:2, borderColor:'darkblue'}}>
+                        <CardMedia sx={{maxWidth:800, m:0, ml:'auto', mr:'auto'}}>
+                            <ResponsiveEmbed src={props.src}  ratio = ' 3:1.5 '/>  
+                        </CardMedia>
+                    </Card>
+                }
                 <Typography sx={{padding:4}} textAlign="center" /*LinkedIn */>
                     <Button variant="outlined" sx={{bgcolor:'white', m:0.3}} href="/">Volver</Button>
                 </Typography>
