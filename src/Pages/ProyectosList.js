cd8565ce6fcb485ae9953c604e9ccd8ffa937a82
Juan <escobar130485@gmail.com>
2022-05-09 21:37:02 -0300

    explicacion

--- src/Pages/ProyectosList.js
+++ src/Pages/ProyectosList.js
@@ -34,8 +34,11 @@ class ProyectosList extends React.Component{
             description: 'Proyecto final de carrera',
             link:"https://www.youtube.com/embed/cBZI4EByklI",
             resenia:"Simulador de manejo de Kitesurf desarrollado en Realidad Virtual como proyecto final de carrera.",
-            parrafo1:`El Kitesurf es un deporte extremo y más del 80% de los accidentes en este deporte ocurren en la etapa inicial. `,
-            parrafo2:"",
+            parrafo1:`El Kitesurf es un deporte extremo y más del 80% de los accidentes en este deporte ocurren en la etapa inicial. 
+            Por ello me propuse como objetivo desarrollar un simulador de manejo inicial de la cometa que permita experimentar las 
+            fuerzas recibidas durante la práctica del deporte`,
+            parrafo2:`Un microcontrolador recibe los movimientos de la barra (inclinación y distancia) permitiendo simular el manejo en un 
+            dispositivo celular. Un servomotor reacciona dependiendo de los movimientos de la barra generando tensión equivalente al kite `,
           },
           {
             id:3,
@@ -43,9 +46,11 @@ class ProyectosList extends React.Component{
             title: 'Videojuego en 3ra Persona',
             description: 'Escenario inspirado en la Facultad',
             link:"https://www.youtube.com/embed/Onx34kHcAC0",
-            resenia:"",
-            parrafo1:"",
-            parrafo2:"",
+            resenia:"Juego en tercera persona cuyo escenario está inspirado en la Facultad de Ingeniería del Ejército",
+            parrafo1:`Este videojuego fue un gran desafío para implementar conocimientos de programación orientada a objetos 
+            en un entorno nuevo.`,
+            parrafo2:`El proposito del desarrollo de estos videojuegos fue adquirir de manera progresiva los conocimientos necesarios 
+            para alcanzar proyectos orientados a la simulación`,
           },
           {
             id:4,
@@ -53,9 +58,10 @@ class ProyectosList extends React.Component{
             title: 'Videojuego en 1ra Persona',
             description: 'Empleado para propuesta de casamiento',
             link:"https://www.youtube.com/embed/jSc5OguxzlM",
-            resenia:"",
-            parrafo1:"",
-            parrafo2:"",
+            resenia:"Desarrollo empleado para propuesta de matrimonio",
+            parrafo1:`"Cuando uno hace lo que ama, no trabaja nunca más".`,
+            parrafo2:`Este proyecto personal fue para poder traerla a mi esposa casa cada ez que necesite. Ahora vamos a estar siempre cerca de 
+            nuestros comienzos, no importa donde nos encontremos `,
           },
           {
             id:5,
@@ -63,8 +69,9 @@ class ProyectosList extends React.Component{
             title: 'Videojuego touch screen',
             description: '@hjrc33',
             link:"https://www.youtube.com/embed/UA3ghy65hb8",
-            resenia:"",
-            parrafo1:"",
+            resenia:"Juego touchscreen para celular",
+            parrafo1:`Este videojuego fue un paso más para reconocer funcionalidades e interacción entre herramientas de desarrollo y plataformas
+            celulares`,
             parrafo2:"",
           },
           {
@@ -73,9 +80,10 @@ class ProyectosList extends React.Component{
             title: 'Proyecto MEDTEMP',
             description: 'Desarrollado para el Ministerio de Defensa Argentino ',
             link:"http://www.escuelasuperiordeguerra.iese.edu.ar/",
-            resenia:"",
-            parrafo1:"",
-            parrafo2:"",
+            resenia:"Proyecto desarrollado en el laboratorio de Mecatrónica Infiniem Labs",
+            parrafo1:"Este proyecto fue lanzado en pandemia y tuvo alcance nacional.",
+            parrafo2:`Con un equipo de trabajo se desarrolló un medidor de temperatura con capacidad de elevar datos a una base y controlar 
+            de esta manera los ingresos en las distintas dependencias del Ministerio de Defensa Argentino.`,
           },
           {
             id:7,
@@ -83,8 +91,9 @@ class ProyectosList extends React.Component{
             title: 'Echair',
             description: 'Prototipo de silla personas con movilidad reducida',
             link:"https://www.youtube.com/embed/L8up2-bKew8",
-            resenia:"",
-            parrafo1:"",
+            resenia:"Prototipo para manejo tactil o por voz de una silla motorizada",
+            parrafo1:`Este proyecto fue realizado con appInventor y Arduino, permitiendo observar el comportamiento del sistema con programación 
+            orientada a objetos en el microcontrolador.`,
             parrafo2:"",
           },
       ]
